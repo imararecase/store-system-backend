f6d60b4fb00437497d8fd06db993693e14dded76
imararecase <danign20@hotmail.com>
2025-08-21 22:31:40 -0500

    Add: AppDbContext
    Se creó la clase AppDbContext con DbSet para Menu, Employee, Customer, Category y Product.

---
+++ store-system-backend/DbContexts/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using store_system_backend.Models;
+
+namespace store_system_backend.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        protected AppDbContext() { }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories {get; set; }
+        public DbSet<Product> Products {get; set; }
+    }
+}
